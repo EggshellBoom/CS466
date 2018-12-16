09ada73241d8672539cc9bf3b19beed3f4440f89
Li <tli67@illinois.edu>
2018-12-16 11:32:13 -0600

    Updates

---
+++ precache-manifest.16f36e4d2aff20c9b231847d60daceb6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "360dfd5b49919c97d91d0a4c187f775e",
+    "url": "/Sequence-Alignment-Tutorial/static/media/Local.360dfd5b.png"
+  },
+  {
+    "revision": "e2fff51cad61ac5d287c3bf392e0e928",
+    "url": "/Sequence-Alignment-Tutorial/static/media/Global.e2fff51c.png"
+  },
+  {
+    "revision": "edd622f22d4bde34052a849d89a0376b",
+    "url": "/Sequence-Alignment-Tutorial/static/media/Fitting.edd622f2.png"
+  },
+  {
+    "revision": "623fe0a48e61fe0684b4",
+    "url": "/Sequence-Alignment-Tutorial/static/js/runtime~main.623fe0a4.js"
+  },
+  {
+    "revision": "f6d8dfed1949f5b2b29a",
+    "url": "/Sequence-Alignment-Tutorial/static/js/main.f6d8dfed.chunk.js"
+  },
+  {
+    "revision": "0a1bf2f9935f067a091e",
+    "url": "/Sequence-Alignment-Tutorial/static/js/1.0a1bf2f9.chunk.js"
+  },
+  {
+    "revision": "f6d8dfed1949f5b2b29a",
+    "url": "/Sequence-Alignment-Tutorial/static/css/main.32f0cfd0.chunk.css"
+  },
+  {
+    "revision": "7ff6165f7b97085905c92b9d708aead5",
+    "url": "/Sequence-Alignment-Tutorial/index.html"
+  }
+];
